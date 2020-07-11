3745bf20d93038b841afaf4615d7dfd7f42f2812
Кирилл Черепанов <ktcherepanov@nologostudio.ru>
2020-07-11 09:03:40 +0300

    add byild

---
+++ client/build/precache-manifest.d8c26b897a9c709291174ee530399a61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f410f3fe05a2f92f838b812fd6d1debb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10dc3228dda80b62f4d4",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "9a0b747cdbc4657b8d83",
+    "url": "/static/css/main.904e19b2.chunk.css"
+  },
+  {
+    "revision": "10dc3228dda80b62f4d4",
+    "url": "/static/js/2.42d7912b.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.42d7912b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0b747cdbc4657b8d83",
+    "url": "/static/js/main.2906eabc.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
